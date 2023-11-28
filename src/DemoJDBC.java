287f44812556b13b6c04e88a04fa891dc6178597
Shyam K <shyamsam1818@gmail.com>
2023-11-28 20:46:48 +0530

    second

--- src/DemoJDBC.java
+++ src/DemoJDBC.java
@@ -39,27 +39,57 @@ public class DemoJDBC {
     }
 
     public static void printAllNames(String url,String userName,String password,String sqlQuery) throws SQLException{
-
-        ResultSet rs;
         Connection conn = DriverManager.getConnection(url, userName, password);
         Statement st=conn.createStatement();
-        rs=st.executeQuery(sqlQuery);
+        ResultSet rs=st.executeQuery(sqlQuery);
 
         while(rs.next()){
             System.out.println(rs.getInt(1)+" : "+rs.getString(2)+" : "+rs.getInt(3));
         }
 
     }
+
+    public static void addDetails(String url,String userName,String password,String sqlQuery) throws SQLException{
+        Connection conn=DriverManager.getConnection(url,userName,password);
+        Statement st=conn.createStatement();
+        st.execute(sqlQuery);
+        System.out.println("Data Successfully Added ");
+    }
+
+    private static void updateDetails(String url, String userName, String password, String sqlQuery) throws SQLException{
+        Connection conn=DriverManager.getConnection(url,userName,password);
+        Statement st=conn.createStatement();
+        st.execute(sqlQuery);
+        System.out.println("Data Successfully Updated ");
+    }
+
+    private static void deleteDetails(String url, String userName, String password, String sqlQuery) throws SQLException{
+        Connection conn=DriverManager.getConnection(url,userName,password);
+        Statement st=conn.createStatement();
+        st.execute(sqlQuery);
+        System.out.println("Data Successfully Updated ");
+    }
+
+
     public static void main(String[] args) throws SQLException {
 
         String url = "jdbc:postgresql://localhost:5432/demo";
         String userName = "postgres";
         String password = "root";
 
-        String sqlQuery1="select name from student where sid=2";
+        System.out.println("Connection Created");
+
+        String sqlQuery1="select name from student where sid=4";
         String sqlQuery2="select * from student";
+        String sqlQuery3="insert into student values(5,'Jimmy',86)";
+        String sqlQuery4="update student set name='jhonny' where sid=3";
+        String sqlQuery5="delete from student where sid=2";
 
         printName(url,userName,password,sqlQuery1);
         printAllNames(url,userName,password,sqlQuery2);
+        addDetails(url,userName,password,sqlQuery3);
+        updateDetails(url,userName,password,sqlQuery4);
+        deleteDetails(url,userName,password,sqlQuery5);
     }
+
 }
