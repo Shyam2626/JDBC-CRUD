e130ddefe24fc696b455bcd1b83f6b2cd208e989
Shyam K <shyamsam1818@gmail.com>
2023-11-29 12:56:41 +0530

    completed

--- src/DemoJDBC.java
+++ src/DemoJDBC.java
@@ -1,17 +1,13 @@
 
 import java.net.SocketOption;
 import java.sql.*;
+import java.util.Scanner;
+
 public class DemoJDBC {
 
     public static void printName(String url,String userName,String password,String sqlQuery) throws SQLException{
 
         ResultSet rs;
-
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
 
         Connection conn;
         try {
@@ -39,6 +35,7 @@ public class DemoJDBC {
     }
 
     public static void printAllNames(String url,String userName,String password,String sqlQuery) throws SQLException{
+        
         Connection conn = DriverManager.getConnection(url, userName, password);
         Statement st=conn.createStatement();
         ResultSet rs=st.executeQuery(sqlQuery);
@@ -53,25 +50,30 @@ public class DemoJDBC {
         Connection conn=DriverManager.getConnection(url,userName,password);
         Statement st=conn.createStatement();
         st.execute(sqlQuery);
-        System.out.println("Data Successfully Added ");
     }
 
     private static void updateDetails(String url, String userName, String password, String sqlQuery) throws SQLException{
         Connection conn=DriverManager.getConnection(url,userName,password);
         Statement st=conn.createStatement();
         st.execute(sqlQuery);
-        System.out.println("Data Successfully Updated ");
     }
 
     private static void deleteDetails(String url, String userName, String password, String sqlQuery) throws SQLException{
         Connection conn=DriverManager.getConnection(url,userName,password);
         Statement st=conn.createStatement();
         st.execute(sqlQuery);
-        System.out.println("Data Successfully Updated ");
     }
 
 
     public static void main(String[] args) throws SQLException {
+
+        Scanner sc=new Scanner(System.in);
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
         String url = "jdbc:postgresql://localhost:5432/demo";
         String userName = "postgres";
@@ -79,17 +81,50 @@ public class DemoJDBC {
 
         System.out.println("Connection Created");
 
-        String sqlQuery1="select name from student where sid=4";
-        String sqlQuery2="select * from student";
-        String sqlQuery3="insert into student values(5,'Jimmy',86)";
-        String sqlQuery4="update student set name='jhonny' where sid=3";
-        String sqlQuery5="delete from student where sid=2";
+        while(true){
 
-        printName(url,userName,password,sqlQuery1);
-        printAllNames(url,userName,password,sqlQuery2);
-        addDetails(url,userName,password,sqlQuery3);
-        updateDetails(url,userName,password,sqlQuery4);
-        deleteDetails(url,userName,password,sqlQuery5);
+            System.out.println("1. Add Details");
+            System.out.println("2. Read Details");
+            System.out.println("3. Update Details");
+            System.out.println("4. Delete Details");
+
+            int choice=sc.nextInt();
+
+            if(choice==1){
+                System.out.println("Enter sid Number");
+                int newSid=sc.nextInt();
+                System.out.println("Enter the name");
+                String newName=sc.next();
+                System.out.println("Enter the mark");
+                int newMark=sc.nextInt();
+                String query = "insert into student values(" + newSid + ", '" + newName + "', " + newMark + ")";
+                addDetails(url,userName,password,query);
+                System.out.println("Successfully Details Added");
+            }
+            else if(choice==2){
+                System.out.println("All the details are..");
+                String sqlQuery="select * from student";
+                printAllNames(url,userName,password,sqlQuery);
+            }
+            else if(choice==3){
+                System.out.println("Enter the name to change");
+                String newName=sc.next();
+                System.out.println("Enter the respective sid Number");
+                int sId=sc.nextInt();
+                String query = "update student set name='" + newName + "' where sid=" + sId;
+                updateDetails(url,userName,password,query);
+                System.out.println("Details Succesfully Updated");
+            }
+            else if(choice ==4){
+                System.out.println("Enter the sid Number to delete the details");
+                int Sid=sc.nextInt();
+                String query="delete from student where sid="+Sid;
+                deleteDetails(url,userName,password,query);
+                System.out.println("Details Succesfully deleted");
+            }else break;
+
+        }
+
     }
 
 }
